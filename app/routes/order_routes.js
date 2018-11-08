b30daca63bb21ad3f7d52172a26d484b84a4fa64
LightCloud36 <dereko.neil@yahoo.com>
2018-11-08 13:04:10 -0500

    Ownership on order INDEX
    added filter method to only return the orders that the user created
    and no one else's.

--- app/routes/order_routes.js
+++ app/routes/order_routes.js
@@ -33,13 +33,22 @@ const router = express.Router()
 router.get('/orders', requireToken, (req, res) => {
   Order.find().populate('products')
     .then(orders => {
+      console.log(req.user.id)
+
+      const ordersByOwner = orders.filter(order => {
+        if (order.owner == req.user.id) {
+          return true
+        }
+      })
+      console.log(ordersByOwner)
       // pass the `req` object and the Mongoose record to `requireOwnership`
       // it will throw an error if the current user isn't the owner
       // requireOwnership(req, orders)
       // `orders` will be an array of Mongoose documents
       // we want to convert each one to a POJO, so we use `.map` to
       // apply `.toObject` to each one
-      return orders.map(order => order.toObject())
+      return ordersByOwner.map(order => order.toObject())
+      // return order.filter()
     })
     // respond with status 200 and JSON of the orders
     .then(orders => res.status(200).json({ orders: orders }))
