cc7d2a88fe8eca8c621370f5d0d363123debb178
ebrault <emmet.brault@gmail.com>
2018-11-08 12:48:00 -0500

    Changed create script

--- scripts/stripe/create.sh
+++ scripts/stripe/create.sh
@@ -3,7 +3,7 @@ URL_PATH="/charges"
 
 curl https://api.stripe.com/v1/charges \
   -u sk_test_k5orqmsiNDI5QaGeGGGzhVmq: \
-  -d amount=1 \
+  -d amount=50 \
   -d currency=usd \
   -d description="example charge" \
   -d source="tok_visa"
